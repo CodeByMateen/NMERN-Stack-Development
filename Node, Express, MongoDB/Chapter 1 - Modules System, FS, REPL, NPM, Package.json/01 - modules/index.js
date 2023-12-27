80f6ece2730912efa0233b8898eeb346947841b3
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-12-28 01:02:37 +0500

    Update index.js

--- Node, Express, MongoDB/Chapter 1 - Modules System, FS, REPL, NPM, Package.json/01 - modules/index.js
+++ Node, Express, MongoDB/Chapter 1 - Modules System, FS, REPL, NPM, Package.json/01 - modules/index.js
@@ -1,5 +1,5 @@
-import {sum, diff} from './lib.js' // for commonjs method import
-// const lib = require('./lib.js') // for ES6 method import
+import {sum, diff} from './lib.js' // for ES6 method import
+// const lib = require('./lib.js') // for commonjs method import
 
 console.log(sum(5,6)) // to run ES6 method
 // console.log(lib.sum(5,6)) // to run commonjs method
