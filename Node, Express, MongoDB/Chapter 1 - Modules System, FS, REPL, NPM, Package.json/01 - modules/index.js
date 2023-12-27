6625dfb2ceb4ebf884a24a4b228add5fe45a2143
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-12-28 01:04:34 +0500

    Update index.js

--- Node, Express, MongoDB/Chapter 1 - Modules System, FS, REPL, NPM, Package.json/01 - modules/index.js
+++ Node, Express, MongoDB/Chapter 1 - Modules System, FS, REPL, NPM, Package.json/01 - modules/index.js
@@ -5,6 +5,5 @@ console.log(sum(5,6)) // to run ES6 method
 // console.log(lib.sum(5,6)) // to run commonjs method
 console.log("Hello")
 
-
 // change type = module if you're following ES6 Method in package.json
 // change type = commonjs if you're following commonjs method in package.json
